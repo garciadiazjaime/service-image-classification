65af74b7b9d9feb9bd0a6162c22c285fc2356727
jaime <garciadiazjaime@gmail.com>
2021-12-10 11:01:04 -0600

    refacor: remove model

--- image-classification.js
+++ image-classification.js
@@ -1,20 +1,15 @@
 const fetch = require('node-fetch');
-const mobilenet = require('@tensorflow-models/mobilenet');
-const tfnode = require('@tensorflow/tfjs-node');
 
 const debug = require('debug')('app:classify-images');
 
 async function getImageClassification(mediaUrl) {
   debug(`classifying:${mediaUrl}`);
-  const mobilenetModel = await mobilenet.load();
 
   const response = await fetch(mediaUrl);
   const imageBuffer = await response.buffer();
-  const tfimage = tfnode.node.decodeImage(imageBuffer);
+  debug(imageBuffer.length);
 
-  const classification = await mobilenetModel.classify(tfimage);
-
-  return classification;
+  return [];
 }
 
 module.exports = getImageClassification;
