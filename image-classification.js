26765a165675394f1112a9eac21167c39623573b
jaime <garciadiazjaime@gmail.com>
2021-12-10 08:50:14 -0600

    refactor: download image same fn

--- image-classification.js
+++ image-classification.js
@@ -4,23 +4,17 @@ const tfnode = require('@tensorflow/tfjs-node');
 
 const debug = require('debug')('app:classify-images');
 
-async function getImage(mediaUrl) {
-  const response = await fetch(mediaUrl);
-  const imageBuffer = await response.buffer();
-  const tfimage = tfnode.node.decodeImage(imageBuffer);
-
-  return tfimage;
-}
-
 async function ImageClassification() {
   const mobilenetModel = await mobilenet.load();
 
   return async function getImageClassification(mediaUrl) {
     debug(`classifying:${mediaUrl}`);
 
-    const image = await getImage(mediaUrl);
+    const response = await fetch(mediaUrl);
+    const imageBuffer = await response.buffer();
+    const tfimage = tfnode.node.decodeImage(imageBuffer);
 
-    const classification = await mobilenetModel.classify(image);
+    const classification = await mobilenetModel.classify(tfimage);
 
     return classification;
   };
